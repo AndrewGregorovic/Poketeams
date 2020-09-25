1ff26f00b63c118486e51b9918d3f2d515216a7f
Andrew Gregorovic <andrew.g.93@hotmail.com>
2020-09-25 12:44:36 +1000

    Coded some additional methods. Not sure if anything works yet, just trying to put code down for what I think is needed.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import requests
 import json
+import
 
+# Additional feature if time, have offline mode to only view saved teams
 class Data():
     team_data_path = "json/team_data.json"
     current_team = None
@@ -12,9 +14,14 @@ class Data():
         except:
             self.team_data = []
 
+    def convert_to_objects(self):
+        pass
+
     def new_team(self):
         new_name = input("What would you like to name this new team?: ")
         self.current_team = Team(new_name)
+        
+        # Additional functionality, warn if name is already used 
 
     def list_saved_teams(self):
         print("Currently saved teams:")
@@ -57,15 +64,33 @@ class Team():
     def __init__(self, name):
         self.name = name
 
+    def view_team(self):
+        print(f"Team: {self.name}\n")
+        for i in range(6):
+            print(f"Pokemon {i + 1}:")
+            if pokemon_list[i]:
+                print(f"    {pokemon_list[i].name}\n")
+            else:
+                print("    Empty")
+
 class Pokemon():
     move_list = [None, None, None, None]
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, response):
+        self.id = None
+        self.name = None
+        self.types = ()
+        self.weight = None
+        self.height = None
+        self.abilities = {}
 
 class APIHandler():
     api_url = "https://pokeapi.co/api/v2/"
 
-    def get_pokemon(self, name):
-        request_url = api_url + "pokemon/" + name
+    def get_pokemon(self, name, query_string = ""):
+        request_url = api_url + "pokemon/" + name + query_string
+        return json.loads(requests.get(request_url).text)
+
+    def get_move(self, name):
+        request_url = api_url + "move/" + name
         return json.loads(requests.get(request_url).text)
