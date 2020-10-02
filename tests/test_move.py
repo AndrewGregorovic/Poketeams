38ad74b5f7341f1c78a27b8e83d2b1f0d7ad4883
Andrew Gregorovic <andrew.g.93@hotmail.com>
2020-10-02 15:05:36 +1000

    Coded the few move unit tests, I think that's all the required stuff I need to do for this project. Now to make sure it all works properly.

--- tests/test_move.py
+++ tests/test_move.py
@@ -5,13 +5,36 @@ import unittest
 
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/src")
 
-# from move import Move  # noqa: E402
+from move import Move  # noqa: E402
 
 
 class TestMoveClass(unittest.TestCase):
 
     def setUp(self):
-        pass
+        """Set defaults for creating test Move objects"""
+        self.default_move = ["None", 0, 0, 0, "None", 0, "None"]
 
-    def test_new_move_instance(self):
-        pass
+    def test_init(self):
+        """Test instantiation"""
+        try:
+            test_move = Move(*self.default_move)
+            test_move.name
+            test_move.accuracy
+            test_move.power
+            test_move.pp
+            test_move.type
+            test_move.effect_chance
+            test_move.effect
+        except Exception as e:
+            self.assertTrue(False, e)
+
+    def test_get_move_options(self):
+        """Test Move.get_move_options()"""
+        test_move = Move(*self.default_move)
+        test_options = test_move.get_move_options("online")
+        self.assertIsInstance(test_options, list, "test options are not being returned as a list of options!")
+        self.assertEqual(len(test_options), 2, "the number of options being returned is not equal to 2!")
+
+        test_options = test_move.get_move_options("offline")
+        self.assertIsInstance(test_options, list, "test options are not being returned as a list of options!")
+        self.assertEqual(len(test_options), 1, "the number of options being returned is not equal to 1!")
